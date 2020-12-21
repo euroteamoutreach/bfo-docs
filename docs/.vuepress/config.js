69d76bcf99987262a5100d373d0cc4f28d9b3e5f
Joshua Steele <joshukraine@gmail.com>
2020-12-21 11:34:50 +0200

    Add favicon.
    * https://vuepress.vuejs.org/config/#head

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'Bible First Online | Docs',
   description: 'Documentation for users of Bible First Online',
+  head: [['link', { rel: 'icon', type: 'image/png', href: '/bf-favicon.png' }]],
   themeConfig: {
     logo: '/bfo-logo-mini.svg',
     nav: [
