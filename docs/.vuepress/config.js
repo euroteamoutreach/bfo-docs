13375298712236528f81996e526394280c6d560a
Joshua Steele <joshukraine@gmail.com>
2020-12-21 11:19:14 +0200

    Fill in top navbar.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
   title: 'Bible First Online | Docs',
   description: 'Documentation for users of Bible First Online',
+  themeConfig: {
+    logo: '/bfo-logo-mini.svg',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'biblefirst.online', link: 'https://biblefirst.online' },
+    ],
+  },
 };
